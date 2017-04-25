c1d85868a1969f6671663ea386e475c6ed909a7d
vanessamuller <vane9090@gmail.com>
2017-04-25 17:38:04 -0400

    built som of the site

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+  $('.offcanvas-toggle').on('click', function() {
+    $('body').toggleClass('offcanvas-expanded');
+  });
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    }
+  }
+
+
+});
